ce9287299e4b110e1c3534f29754495bb2f33e60
kefmans123 <96120635+kefmans123@users.noreply.github.com>
2022-06-23 10:36:39 +0200

    Upload Func. Show Username

---
+++ Show username function/ShowUsername.php
@@ -0,0 +1,22 @@
+function give_username(){
+    $user=wp_get_current_user();
+    $name=$user->display_name; 
+    return $name;
+}
+
+add_shortcode('profile_name', 'give_username');
+
+add_filter( 'wp_nav_menu_objects', 'my_dynamic_menu_items' );
+function my_dynamic_menu_items( $menu_items ) {
+    foreach ( $menu_items as $menu_item ) {
+        if ( '#profile_name#' == $menu_item->title ) {
+            global $shortcode_tags;
+            if ( isset( $shortcode_tags['profile_name'] ) ) {
+                // Or do_shortcode(), if you must.
+                $menu_item->title = call_user_func( $shortcode_tags['profile_name'] );
+            }    
+        }
+    }
+
+    return $menu_items;
+} 
